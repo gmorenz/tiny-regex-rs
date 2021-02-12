e732572256357265d7cfcbeb11b3f91978ed4df6
Greg Morenz <morenzg@gmail.com>
2021-02-12 06:31:54 -0500

    Add a rough benchmark

--- rough_bench.sh
+++ rough_bench.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-set -ex
+set -e
+
+# Bench Rust
+
 if [ ! -d tiny-regex-c ]; then
     git clone https://github.com/gmorenz/tiny-regex-c --branch rust_build
 fi
@@ -7,4 +10,13 @@ cargo build --example tiny_regex_rs --release
 cp target/release/examples/libtiny_regex_rs.a tiny-regex-c
 cd tiny-regex-c
 git checkout rust_build
-make test
+make rust_all
+echo "Rust time" > ../timings
+/usr/bin/time --output=../timings --append make test
+
+# Bench C
+
+git checkout master
+make all
+echo "C time" >> ../timings
+/usr/bin/time --output=../timings --append make test
